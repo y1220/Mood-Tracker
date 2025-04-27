27da94106e3b8f5b33dc1fd3ebbb939491c2c3ae
y1220 <ga.2.y1220@gmail.com>
2025-04-27 13:19:53 +0200

    feat(PageID): add link on title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Generate a URL to a Notion page
+  def notion_page_url(page_id)
+    return nil unless page_id.present?
+
+    # Notion page URLs are in the format: https://notion.so/[workspace]/[page-id]
+    # The workspace part is optional, so we'll use the simple format
+    "https://notion.so/#{page_id.gsub('-', '')}"
+  end
 end
